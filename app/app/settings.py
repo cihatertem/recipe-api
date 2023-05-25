cf8f3318a02477ef96aba587eee0b53b75b90ff8
Cihat Ertem <cihatertem@gmail.com>
2023-05-25 20:27:03 +0300

    settings.py DEBUG & ALLOWED_HOSTS depends on DEBUG_MODE

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG_MODE")
+DEBUG = True if os.getenv("DEBUG_MODE") == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] if DEBUG == True else ["*"]
 
 
 # Application definition
